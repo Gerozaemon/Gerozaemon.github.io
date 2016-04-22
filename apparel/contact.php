da6397798ac0c47c945604bf675a03a49e5a726a
Gerozaemon <delta_brother_gerozaemon@live.jp>
2016-04-22 13:17:05 +0900

    修正版0422

--- apparel/contact.php
+++ apparel/contact.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="author" content="Art-Studio '2012 (c) Karaush A." />
-<?php include 'material/tpl/default/meta.php' ?>
+<?php include 'material/tpl/default/metasub.php' ?>
 <script type="text/javascript" src="material/js/jquery-1.7.2.min.js"></script>
 <script type="text/javascript" src="material/js/jquery.validationEngine.js"></script>
 <script type="text/javascript" src="material/js/languages/jquery.validationEngine-ja.js"></script>
@@ -20,13 +20,9 @@
     <?php include 'material/tpl/default/header_2.php' ?>
   </header>
   <div class="content">
-
-
       <?php include 'page/contact.html' ?>
-
-
-    <div class="empty"> &nbsp; </div>
-  </div>
+  <div class="empty"> &nbsp; </div>
+</div>
 </div>
 <footer id="footer">
   <div class="content">
--- apparel/login.php
+++ apparel/login.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="author" content="Art-Studio '2012 (c) Karaush A." />
-<?php include 'material/tpl/default/meta.php' ?>
+<?php include 'material/tpl/default/metasub.php' ?>
 <link rel="stylesheet" href="material/tpl/default/css/base.css" type="text/css" />
 <link rel="stylesheet" href="material/tpl/default/css/jquery.jscrollpane.css" type="text/css"/>
 <link rel="stylesheet" href="material/tpl/default/css/validationEngine.jquery.css" type="text/css" />
@@ -12,6 +12,7 @@
 <script type="text/javascript" src="material/js/jquery-1.7.2.min.js"></script>
 <script type="text/javascript" src="material/js/jquery.validationEngine.js"></script>
 <script type="text/javascript" src="material/js/languages/jquery.validationEngine-ja.js"></script>
+
 
 
 
